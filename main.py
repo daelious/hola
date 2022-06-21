e6160be62882e1d065a570e4be2058ee2742fd51
Keith Meyer <meyer.keith@gmail.com>
2022-06-21 12:48:48 -0400

    Add research paper and adjust a couple of parameters.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-from cv2 import grabCut
+import numpy as np 
 
 print(cv.__version__)
 
@@ -14,7 +14,12 @@ while True:
     face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30))
+    faces = face_cascade.detectMultiScale(
+        gray, 
+        scaleFactor=1.2, 
+        minNeighbors=5, 
+        minSize=(30, 30)
+    )
 
     for (x, y, w, h) in faces:
         cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
