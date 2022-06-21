a34a6562a733799e4d8dd26b763f0ff9ab1f3c0f
Keith Meyer <meyer.keith@gmail.com>
2022-06-21 12:22:11 -0400

    Facial recognition pieces, outlines faces discovered in a blue rectangle

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 import cv2 as cv
-import numpy as np
-
+from cv2 import grabCut
+# import numpy as np
 
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open source camera")
     exit()
 
+
 while True:
     _, frame = cap.read()
+    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    lower_green = np.array([36, 25, 25])
-    upper_green = np.array([86, 255, 255])
-    green_mask = cv.inRange(hsv, lower_green, upper_green)
-
-
-    lower_blue = np.array([110, 50, 50])
-    upper_blue = np.array([130, 255, 255])
-    blue_mask = cv.inRange(hsv, lower_blue, upper_blue)
-
-    mask = cv.bitwise_or(blue_mask, green_mask)
-
-    res = cv.bitwise_and(frame, frame, mask=mask)
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
     cv.imshow('frame', frame)
-    cv.imshow('mask', mask)
-    cv.imshow('res', res)
 
-    # k = cv.waitKey(5) & 0xFF
-    # if k == 27:
-    #     break
-    
     if cv.waitKey(1) == ord('q'):
         break
 
