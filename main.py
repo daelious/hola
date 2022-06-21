62cc9f8d200b465a41bd18453b62e123b3f0971b
Keith Meyer <meyer.keith@gmail.com>
2022-06-21 12:25:14 -0400

    Better parameters

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from sys import flags
 import cv2 as cv
 from cv2 import grabCut
+import cv2
 # import numpy as np
 
 cap = cv.VideoCapture(0)
@@ -13,7 +15,7 @@ while True:
     face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30))
 
     for (x, y, w, h) in faces:
         cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
