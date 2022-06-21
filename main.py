34de8f59e4079994efbddec4366421bfcf223c48
Keith Meyer <meyer.keith@gmail.com>
2022-06-21 11:34:21 -0400

    Detect multiple colors.

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ while True:
 
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
+    lower_green = np.array([36, 25, 25])
+    upper_green = np.array([86, 255, 255])
+    green_mask = cv.inRange(hsv, lower_green, upper_green)
+
+
     lower_blue = np.array([110, 50, 50])
     upper_blue = np.array([130, 255, 255])
+    blue_mask = cv.inRange(hsv, lower_blue, upper_blue)
 
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    mask = cv.bitwise_or(blue_mask, green_mask)
 
     res = cv.bitwise_and(frame, frame, mask=mask)
 
@@ -23,9 +29,9 @@ while True:
     cv.imshow('mask', mask)
     cv.imshow('res', res)
 
-    k = cv.waitKey(5) & 0xFF
-    if k == 27:
-        break
+    # k = cv.waitKey(5) & 0xFF
+    # if k == 27:
+    #     break
     
     if cv.waitKey(1) == ord('q'):
         break
